a64f063583879f58f5818a56d5818431d0d8ee3f
Arman Jon Villalobos <me@armanjon.com>
2017-05-12 19:29:09 +0800

    Create Client.js for calling the test-api methods

---
+++ src/Client.js
@@ -0,0 +1,24 @@
+function getAllDevices(callback) {
+  return fetch(`/api/v1/devices`, {
+    accept: 'application/json',
+  }).then(checkStatus)
+    .then(parseJSON)
+    .then(callback);
+}
+
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  const error = new Error(`HTTP Error ${response.status}`);
+  error.status = response.status;
+  error.response = response;
+  throw error;
+}
+
+function parseJSON(response) {
+  return response.json();
+}
+
+const Client = { getAllDevices };
+export default Client;
